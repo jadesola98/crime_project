60f2c52315fba9bab16663b94d5893b94418ac75
jadesola98 <77394784+jadesola98@users.noreply.github.com>
2021-09-21 14:26:07 +0100

    Create source_data_script.py

---
+++ source_data_script.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sodapy import Socrata
+
+client = Socrata("data.cityofchicago.org",
+                 "9GI35TGzZVKetBnn5VRnZiUax",
+                 username="alayandeay@gmail.com",
+                password="UpcwPk$s3W5@acG")
+
+#print(client)
+results = client.get("ijzp-q8t2", limit=5000)
+
+#print(type(results))
+#print(results)
+
+df = pd.DataFrame.from_records(results)
+
+#print(df)
+
+
+from sqlalchemy import create_engine
+engine = create_engine('postgresql://mxzqmels:KY3Gj6B6yFPH1H4eSlmPtmpdwo6SJaJK@chunee.db.elephantsql.com:5432/mxzqmels')
+df.to_sql('city_source_data', engine)
